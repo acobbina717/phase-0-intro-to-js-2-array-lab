f37c52365a8fda75f52af849ddca2f8b4d7a6023
Abeku <acobbina717@gmail.com>
2022-04-05 09:37:37 -0400

    commit tree 4f70fbdf2d17cd41221001c06732ec98d0576ca1

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+// cats.push("Jimmy")
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+}
+function appendCat(name) {
+    let newCats = [...cats]
+    newCats.push(name)
+    return newCats
+}
+function prependCat(name) {
+    let newCats = [...cats]
+    newCats.unshift(name)
+    return newCats
+}
+function removeLastCat(name) {
+    let newCats = [...cats]
+    newCats.pop()
+    return newCats
+}
+function removeFirstCat(name) {
+    let newCats = [...cats]
+    newCats.shift()
+    return newCats
+}
